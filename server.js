3c5f89ea27729aec7ec71f9921c41e84a4b1a304
dimphom@qode.healthcare <dimphomahoko@gmail.com>
2020-06-18 15:56:12 +0200

    final

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
     }
   });
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/calculator'));
 
 app.get('/*', function(req,res) {
     
